e9abf5c81d853d89257616dad177901af018e6ff
EmersonSantana1994 <emerson1994vp@gmail.com>
2023-09-19 10:54:20 -0300

    projeto para teste

---
+++ empresa-desafio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TelaLogin from './componentes/login/login';
+
+function App() {
+  return (
+    <div className="App">
+<TelaLogin></TelaLogin>
+    </div>
+  );
+}
+
+export default App;
